c5cdb7af4e08c11f4130d175c0f18edb3aef2ed7
Nathan louissaint <louissaintmark@gmail.com>
2023-01-13 19:48:14 -0500

    added resume

--- src/components/header/cta.jsx
+++ src/components/header/cta.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Bali from '../../assests/bali.pdf'
+
 
 const cta = () => {
   return (
     <div className="cta">
-      <a href="{Bali}" download={Bali} className="btn">Download CV</a>
+      <a href="https://docs.google.com/document/d/1HaBQsIzwDGPc9UbyUSeeqTr2AHTyxESt/edit" className="btn">View Resume</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
